3014a00ac399f64941362fa597cc5eabd79ee656
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-05-31 07:58:50 -0700

    fix styling, cleanup

--- models/operator.js
+++ models/operator.js
@@ -1,13 +1,5 @@
 var mongoose = require('mongoose');
 
-// The factSchema is used to embedded docs in as tudent doc.
-// There is no model and no 'facts' collection
-// var factSchema = new mongoose.Schema({
-//   text: String
-// }, {
-//   timestamps: true
-// });
-
 var operatorSchema = new mongoose.Schema({
   name: String,
   cohort: String,
