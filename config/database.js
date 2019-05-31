3f61fe360405eb7b071ce79cc79c296c3565edfb
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-05-31 02:47:54 -0700

    Fix heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://sei:kDwsIarHYXHyaln3@sei-mmuyv.azure.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
 
 // database connection event
 mongoose.connection.on('connected', function () {
